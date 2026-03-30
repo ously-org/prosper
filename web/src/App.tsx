7cf016df7746a59b95586f69bdb0aef29c7010b9
preamza02 <preamza02@gmail.com>
2026-03-30 12:55:13 +0700

    chore: update project dependencies in node_modules

---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { AppSidebar } from "./components/app-sidebar";
+
+function App() {
+  return <AppSidebar>hello</AppSidebar>;
+}
+
+export default App;
---
+++ web/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+import { cloudflare } from "@cloudflare/vite-plugin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), cloudflare()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
